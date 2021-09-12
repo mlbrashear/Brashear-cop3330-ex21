855beaf7b39a6fd09573ed3083d471ff81cdbf0b
mlbrashear <brashear.mary@knights.ucf.edu>
2021-09-12 15:32:13 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,53 @@
+package org.example;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Mary Brashear
+ */
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        //declare variables
+        int num;
+        Scanner sc = new Scanner(System.in);
+
+        //scan user input
+        System.out.println("Please enter the number of the month:");
+        num = sc.nextInt();
+
+        //use switch case to determine month
+        String month;
+        switch (num) {
+            case 1:  month = "January";
+                break;
+            case 2:  month = "February";
+                break;
+            case 3:  month = "March";
+                break;
+            case 4:  month = "April";
+                break;
+            case 5:  month = "May";
+                break;
+            case 6:  month = "June";
+                break;
+            case 7:  month = "July";
+                break;
+            case 8:  month = "August";
+                break;
+            case 9:  month = "September";
+                break;
+            case 10: month = "October";
+                break;
+            case 11: month = "November";
+                break;
+            case 12: month = "December";
+                break;
+            default: month = "Invalid month";
+                break;
+        }
+        //print findings
+        System.out.println("The name of the month is " + month + ".");
+
+        sc.close();
+    }
+}
